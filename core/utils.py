fb54d7286504041a919c24b2d2f3b950c9ccf317
Chris George <chrisg1622@gmail.com>
2020-08-19 14:11:25 +0100

    cleanup

--- core/utils.py
+++ core/utils.py
@@ -9,29 +9,23 @@ def compute_avg_return(environment, policy, num_episodes=10):
     for _ in range(num_episodes):
         time_step = environment.reset()
         episode_return = 0.0
-
         while not time_step.is_last():
             action_step = policy.action(time_step)
             time_step = environment.step(action_step.action)
             episode_return += time_step.reward
-
         total_return += episode_return
     avg_return = total_return / num_episodes
     return avg_return.numpy()[0]
 
 
-def collect_step(environment, policy, buffer):
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
-    next_time_step = environment.step(action_step.action)
-    traj = trajectory.from_transition(time_step, action_step, next_time_step)
-    # Add trajectory to the replay buffer
-    buffer.add_batch(traj)
-
-
-def collect_data(env, policy, buffer, steps):
+def collect_data(environment, policy, buffer, steps):
     for _ in range(steps):
-        collect_step(env, policy, buffer)
+        time_step = environment.current_time_step()
+        action_step = policy.action(time_step)
+        next_time_step = environment.step(action_step.action)
+        traj = trajectory.from_transition(time_step=time_step, action_step=action_step, next_time_step=next_time_step)
+        # Add trajectory to the replay buffer
+        buffer.add_batch(traj)
 
 
 def embed_mp4_in_notebook(filename):
@@ -47,8 +41,7 @@ def embed_mp4_in_notebook(filename):
 
 
 def create_policy_eval_video(eval_env, eval_py_env, policy, filename, num_episodes=5, fps=30):
-    filename = filename + ".mp4"
-    with imageio.get_writer(filename, fps=fps) as video:
+    with imageio.get_writer(f'{filename}.mp4', fps=fps) as video:
         for _ in range(num_episodes):
             time_step = eval_env.reset()
             video.append_data(eval_py_env.render())
