92901c12700bc3fc9c3cd650d231819bddc4d2ed
Chris George <chrisg1622@gmail.com>
2020-08-19 13:48:02 +0100

    added script for training cartpole agent

---
+++ cartpole/utils.py
@@ -0,0 +1,59 @@
+import base64
+import imageio
+import IPython
+from tf_agents.trajectories import trajectory
+
+
+def compute_avg_return(environment, policy, num_episodes=10):
+    total_return = 0.0
+    for _ in range(num_episodes):
+        time_step = environment.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+
+        total_return += episode_return
+    avg_return = total_return / num_episodes
+    return avg_return.numpy()[0]
+
+
+def collect_step(environment, policy, buffer):
+    time_step = environment.current_time_step()
+    action_step = policy.action(time_step)
+    next_time_step = environment.step(action_step.action)
+    traj = trajectory.from_transition(time_step, action_step, next_time_step)
+    # Add trajectory to the replay buffer
+    buffer.add_batch(traj)
+
+
+def collect_data(env, policy, buffer, steps):
+    for _ in range(steps):
+        collect_step(env, policy, buffer)
+
+
+def embed_mp4_in_notebook(filename):
+    """Embeds an mp4 file"""
+    video = open(filename, 'rb').read()
+    b64 = base64.b64encode(video)
+    tag = '''
+    <video width="640" height="480" controls>
+    <source src="data:video/mp4;base64,{0}" type="video/mp4">
+    Your browser does not support the video tag.
+    </video>'''.format(b64.decode())
+    return IPython.display.HTML(tag)
+
+
+def create_policy_eval_video(eval_env, eval_py_env, policy, filename, num_episodes=5, fps=30):
+    filename = filename + ".mp4"
+    with imageio.get_writer(filename, fps=fps) as video:
+        for _ in range(num_episodes):
+            time_step = eval_env.reset()
+            video.append_data(eval_py_env.render())
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = eval_env.step(action_step.action)
+                video.append_data(eval_py_env.render())
+
